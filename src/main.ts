7af2bcb54bb4a7e3f914d36ee30f1f95237b8cc7
Antoine Poppe <antoinepoppe@hotmail.fr>
2024-03-27 08:48:02 +0100

    change port to 8000

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(6000);
+
+  app.enableCors({
+    origin: true,
+  });
+
+  await app.listen(6666);
 }
 bootstrap();
